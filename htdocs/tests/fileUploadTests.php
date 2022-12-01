5f2e0cbdae9b3f0b9787da3efeaf33a7b07b946a
milldev <milldev@iastate.edu>
2022-11-30 20:27:13 -0600

    created test folder

---
+++ htdocs/tests/fileUploadTests.php
@@ -0,0 +1,16 @@
+<?php 
+require_once('../fileUploadScript.php');
+
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class UploadTest extends TestCase{
+	
+	
+	public function 
+	
+	
+}
+
+
+?>
