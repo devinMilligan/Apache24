82f226e8d60047179d087bb7125178c807d62bef
milldev <milldev@iastate.edu>
2022-11-30 20:14:29 -0600

    reformatted fileuploadscript

--- htdocs/fileUploadScript.php
+++ htdocs/fileUploadScript.php
@@ -1,62 +1,200 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
 
-$script = new UploadScript();
-$script->username = $_GET['username'];
+$script = new UploadScript($_GET['username']);
 
 header( "refresh:5;url=upload.php?username={$script->username}" );
 
+$script->createDirectory();
+$script->getFileInformation();
+
+if (isset($_POST['submit'])) {
+		if(!$script->isFileExtAllowed()){
+			
+			printErrors($script->errors);
+			exit();
+		}
+		
+		if(!$script->isFileSizeOk()){
+			
+			printErrors($script->errors);
+			exit();
+		}
+		
+		$script->createNumFile();
+		
+		if(!$script->uploadFile()){
+			
+			echo "An error occurred while uploading the file";
+			exit();
+		}
+		
+		if(!$script->addKeywords($_POST['keywords'])){
+			
+			echo "Please add some keywords to upload picture";
+			exit();
+		}
+		
+		echo "The file has been uploaded";
+
+}
+
+function printErrors(array $errs){
+	
+	foreach ($errs as $error) {
+          echo $error . "These are the errors" . "\n";
+    }
+	
+}
+
 class UploadScript{
 	
+	public array $fileExtensionsAllowed = ['jpeg','jpg','png']; // These will be the only file extensions allowed 
+
 	public string $username;
 	public string $currentDirectory;
 	public string $uploadDirectory;
 	public string $num_file;
 	public string $index_file;
-	public string $errors;
+	public array $errors = [];
+	public int $numFile;
 	
-	public function __construct(){
+	
+    public string $fileName;
+    public int $fileSize;
+    public string $fileTmpName;
+    public string $fileType;
+    public string $fileExtension;
+
+	
+	public function __construct(string $usern){
 		
-		$currentDirectory = getcwd();
-		$uploadDirectory = "/images/" . $username . "/";
+		$this->username = $usern;
+		$this->numFile = 0;
+		$this->errors = [];
+		$this->currentDirectory = getcwd();
+		$this->uploadDirectory = "/images/" . $usern . "/";
+		$this->num_file = $this->currentDirectory . $this->uploadDirectory . "num_imgs.txt";
+		$this->index_file =  $this->currentDirectory . $this->uploadDirectory . "index.txt";
 		
 	}
 	
 	public function createDirectory(): bool{
-		
-		if(!is_dir($uploadDirectory)){
-
+				
+		if(!is_dir("." . $this->uploadDirectory)){
+			mkdir("." . $this->uploadDirectory);
 			return true;
 		}
 		return false;
 	}
 	
-	
-}
-	
-	if(!$script->doesFileExist("./images/" . $username . "/")){
+	public function getFileInformation(){
+		
+		
+		$this->fileName = $_FILES['the_file']['name'];
+		$this->fileSize = $_FILES['the_file']['size'];
+		$this->fileTmpName  = $_FILES['the_file']['tmp_name'];
+		$this->fileType = $_FILES['the_file']['type'];
+		$this->fileExtension = strtolower(end(explode('.',$this->fileName)));
 		
 	}
-
-
-
-	$num_file = $currentDirectory . $uploadDirectory . "num_imgs.txt";
-	$index_file =  $currentDirectory . $uploadDirectory . "index.txt";
 	
-	$numFile = 0;
+	public function isFileExtAllowed(): bool{
+		
+		if (! in_array($this->fileExtension,$this->fileExtensionsAllowed)) {
+			$this->errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+			return false;
+		}
+		return true;
+	}
 	
+	public function isFileSizeOk(): bool{
+		
+		if ($this->fileSize > 4000000) {
+			$this->errors[] = "File exceeds maximum size (4MB)";
+			return false;
+		}
+		return true;
+		
+	}
+	
+	public function createNumFile(): bool{
+		if(!is_file($this->num_file)){
+		
+			file_put_contents($this->num_file, $this->numFile+1);
+			$this->uploadPath = $this->currentDirectory . $this->uploadDirectory . $this->numFile . "." . $this->fileExtension; 
 
-    $errors = []; // Store errors here
+			return true;
+		
+		}else{
+			$myfile = fopen($this->num_file, "r");
+			$this->numFile = fread($myfile,filesize($this->num_file));
+			fclose($myfile);
+			file_put_contents($this->num_file, ($this->numFile)+1);
+			$this->uploadPath = $this->currentDirectory . $this->uploadDirectory . $this->numFile . "." . $this->fileExtension; 
+			
+			return false;
+		}
+		
+	}
+	
+	public function uploadFile(): bool{
+		
+		return move_uploaded_file($this->fileTmpName, $this->uploadPath);
+		
+	}
+	
+	public function addKeywords(string $in_keywords): bool{
+		
+		if ( ! empty($in_keywords)){
+			if(!is_file($this->index_file)){
+				$contents = "File Header";
+				file_put_contents($this->index_file, $contents);
+			}
+			$delimStr = str_replace(' ', '', $in_keywords);
+			$keywords = explode(",", $delimStr);
+			$num_keys = count($keywords);
+			$lines = file($this->index_file, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
+			
+			$row_count = 0;
+			
+			foreach($keywords as $key) {
+				$check = 0;
+				foreach($lines as $ind => $line){
+					$line_temp = str_replace(' ', '', $line);
+					$lineArr = explode(",", $line_temp);
+					if($key == $lineArr[0]){
+						$lines[$ind] .= "," . $this->numFile;
+						$check = 1;
+					}
+				}
+				if($check==0){
+					$line_count = count($lines);
+					if($line_count==0){
+						$lines[$row_count] = $key . "," . $this->numFile;
+						$row_count = $row_count + 1;
+					}else{
+						array_push($lines,$key . "," . $this->numFile);
+					}
+				}
+			}
+			
+			file_put_contents($this->index_file, implode(PHP_EOL, $lines));
+				
+			return true;
+				
+		}else{
+			
+			return false; 
+			
+		}
+		
+		
+	}
+	
+}
 
-    $fileExtensionsAllowed = ['jpeg','jpg','png']; // These will be the only file extensions allowed 
-
-    $fileName = $_FILES['the_file']['name'];
-    $fileSize = $_FILES['the_file']['size'];
-    $fileTmpName  = $_FILES['the_file']['tmp_name'];
-    $fileType = $_FILES['the_file']['type'];
-    $fileExtension = strtolower(end(explode('.',$fileName)));
-
-    if (isset($_POST['submit'])) {
+ /*   if (isset($_POST['submit'])) {
 
       if (! in_array($fileExtension,$fileExtensionsAllowed)) {
         $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
@@ -135,5 +273,5 @@ class UploadScript{
         }
       }
 
-    }
+    }*/
 ?>
